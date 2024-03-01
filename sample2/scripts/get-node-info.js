a71a6c1d311e86193171388cdf3defbcb166d450
drumfeet <drumfeet@gmail.com>
2024-03-01 11:40:04 +0800

    update get-node-info script

--- sample2/scripts/get-node-info.js
+++ sample2/scripts/get-node-info.js
@@ -3,6 +3,7 @@ const WeaveDB = require("weavedb-node-client")
 const db = new WeaveDB({
   rpc: "localhost:8080",
   contractTxId: "CONTRACT_TX_ID", // Replace 'CONTRACT_TX_ID' with an existing deployed database contract on the rollup node
+  // contractTxId can be removed and still be able to make a query `db.node()`
 })
 
 const main = async () => {
