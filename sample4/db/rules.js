d717e3954a94f587aa683fbb79f0a37bed99f5e1
drumfeet <drumfeet@gmail.com>
2024-03-12 16:14:32 +0800

    wip

--- sample4/db/rules.js
+++ sample4/db/rules.js
@@ -1,26 +1,72 @@
 module.exports = {
-    posts: [
+  posts: [
+    [
+      "add:post", // define a custom query
       [
-        "add:post", // define a custom query
+        ["fields()", ["*body"]], // only allow "body" field, * makes it mandatory
         [
-          ["fields()", ["*body"]], // only allow "body" field, * makes it mandatory
-          [
-            "mod()", // auto assign some fields
-            {
-              id: "$id", // tx id
-              owner: "$signer", // tx signer
-              date: "$ms", // tx date in millisecond
-            },
-          ],
-          ["allow()"], // allow the query
+          "mod()", // auto assign some fields
+          {
+            id: "$id", // tx id
+            owner: "$signer", // tx signer
+            date: "$ms", // tx date in millisecond
+          },
         ],
-      ],
-      [
-        "delete:post", // define a custom query for deletion
-        [ // "=$" will assign the result of the following FPJSON to a variable
-          ["=$isOwner", ["equals", "$signer", "$old.owner"]], // check if signer is data owner
-          ["allowifall()", ["$isOwner"]], // allow the query if $isOwner is true
-        ],
+        ["allow()"], // allow the query
       ],
     ],
-  }
+    [
+      "set:post", // define a custom query
+      [
+        ["fields()", ["*body"]], // only allow "body" field, * makes it mandatory
+        [
+          "mod()", // auto assign some fields
+          {
+            id: "$id", // tx id
+            owner: "$signer", // tx signer
+            date: "$ms", // tx date in millisecond
+          },
+        ],
+        ["allow()"], // allow the query
+      ],
+    ],
+    [
+      "update:post", // define a custom query
+      [
+        ["fields()", ["*body"]], // only allow "body" field, * makes it mandatory
+        [
+          "mod()", // auto assign some fields
+          {
+            id: "$id", // tx id
+            owner: "$signer", // tx signer
+            date: "$ms", // tx date in millisecond
+          },
+        ],
+        ["allow()"], // allow the query
+      ],
+    ],
+    [
+      "upsert:post", // define a custom query
+      [
+        ["fields()", ["*body"]], // only allow "body" field, * makes it mandatory
+        [
+          "mod()", // auto assign some fields
+          {
+            id: "$id", // tx id
+            owner: "$signer", // tx signer
+            date: "$ms", // tx date in millisecond
+          },
+        ],
+        ["allow()"], // allow the query
+      ],
+    ],
+    [
+      "delete:post", // define a custom query for deletion
+      [
+        // "=$" will assign the result of the following FPJSON to a variable
+        ["=$isOwner", ["equals", "$signer", "$old.owner"]], // check if signer is data owner
+        ["allowifall()", ["$isOwner"]], // allow the query if $isOwner is true
+      ],
+    ],
+  ],
+}
