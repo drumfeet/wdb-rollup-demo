117620989b7c83aa3a42b4e9afa78126c081f6fe
drumfeet <drumfeet@gmail.com>
2024-03-14 17:01:07 +0800

    wip

---
+++ sample4/synchronous/measure-add-performance2.js
@@ -0,0 +1,47 @@
+const { performance, PerformanceObserver } = require("perf_hooks")
+const SDK = require("weavedb-node-client")
+const crypto = require("crypto")
+
+const TX_COUNT = parseInt(process.argv[2], 10) || 1
+const COLLECTION_NAME = process.argv[3] || "posts"
+const CONTRACT_TX_ID = process.argv[4] || "drumtest1"
+const RPC_NODE = process.argv[5] || "13.212.91.15:8080"
+const userAuth =
+  process.argv[6] || require("../.weavedb/accounts/evm/user.json")
+
+const main = async () => {
+  const db = new SDK({
+    rpc: RPC_NODE,
+    contractTxId: CONTRACT_TX_ID,
+  })
+
+  const measureAddPerformance = async (count) => {
+    const durations = []
+    for (let i = 0; i < count; i++) {
+      const randomBytes = crypto.randomBytes(16).toString("hex")
+      try {
+        const start = performance.now()
+        const tx = await db.query(
+          "add:post",
+          { body: `Post ${randomBytes}` },
+          COLLECTION_NAME,
+          userAuth
+        )
+        const end = performance.now()
+
+        durations.push(end - start)
+        console.log("tx.docID", tx.docID)
+      } catch (e) {
+        console.error(e.message)
+      }
+    }
+    const totalDuration = durations.reduce((acc, curr) => acc + curr, 0)
+    const averageLatency = totalDuration / count
+    console.log("durations", durations)
+    console.log(`Average latency: ${averageLatency} milliseconds.`)
+    console.log(`TPS ${count / (totalDuration / 1000)}`)
+  }
+
+  measureAddPerformance(TX_COUNT)
+}
+main()
