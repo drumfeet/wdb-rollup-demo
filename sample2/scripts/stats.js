1af4753561d15295084056ca564f02319e381c9a
drumfeet <drumfeet@gmail.com>
2024-03-20 13:17:44 +0800

    wip

--- sample2/scripts/stats.js
+++ sample2/scripts/stats.js
@@ -2,7 +2,7 @@ const SDK = require("weavedb-node-client")
 
 const main = async () => {
   const db = new SDK({
-    rpc: "test3-1710822176.raas.weavedb-node.xyz:8080",
+    rpc: "test3-1710822176.raas.weavedb-node.xyz:9090",
   })
 
   const stats = await db.node({ op: "stats" })
