f70817bc782bc6a52893e50b084555267b19ace1
drumfeet <drumfeet@gmail.com>
2024-04-16 18:14:06 +0800

    delete sample2

--- sample2/scripts/sample.js
+++
@@ -1,9 +0,0 @@
-const DB = require("weavedb-node-client")
-const db = new DB({ rpc: "localhost:8080", contractTxId: "dbsample1" })
-
-const main = async () => {
-  const stats = await db.node({ op: "stats" })
-  console.log("stats", stats)
-}
-
-main()
