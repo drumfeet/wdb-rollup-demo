a0e209f8f066c90575611137b96efbecf94a4dc5
drumfeet <drumfeet@gmail.com>
2024-05-23 16:37:34 +0800

    wip

--- rollup-demo/test/rate-limit-no-api-key.js
+++ rollup-demo/test/rate-limit-no-api-key.js
@@ -2,13 +2,12 @@ const { expect } = require("chai")
 const SDK = require("weavedb-sdk-node")
 const fs = require("fs")
 const { resolve } = require("path")
-require("dotenv").config({ path: resolve(__dirname, "./.env") })
-const EthCrypto = require("eth-crypto")
 
 describe("Rate Limit Error 429", function () {
   this.timeout(0)
   const COLLECTION_NAME = "posts"
   const CONTRACT_TX_ID = "cxcIF4N8SEyt7PnUgMssZ-HZVNv_LY-iOGgolmRIfQ0"
+  const DOC_ID_TEST = "0xcfe3e0e5b16d81e03ea2c4321b95f256ace3ab8c"
 
   before(async () => {
     this.bail(true)
@@ -32,8 +31,10 @@ describe("Rate Limit Error 429", function () {
       })
       await db.init()
 
-      const txGet = await db.get(COLLECTION_NAME)
-      console.log(txGet)
+      console.log("waiting to fetch docs from db collection.....")
+      const txGetDocs = await db.get(COLLECTION_NAME, DOC_ID_TEST)
+      console.log("txGetDocs", txGetDocs)
+      expect(txGetDocs.address).to.eql(DOC_ID_TEST)
     } catch (e) {
       console.error(e)
       expect(e).to.eql(null)
